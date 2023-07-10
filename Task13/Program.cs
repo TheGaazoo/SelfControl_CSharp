3fbc8efb9a4d329073d6be89f12816f435499b2a
gaazoo <gaazoo@yandex.ru>
2023-07-10 22:55:03 +0700

    Задания готовы.

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = ReadInt("Введите число: ");
+int count = number.ToString().Length;
+Console.WriteLine(MakeArray(number, count));
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int MakeArray(int a, int b)
+{
+int result = 0;
+    if (b < 3)
+    {
+        Console.WriteLine("Третьей цифры нет, держи: ");
+    }
+    else
+    {
+        int c = 1;
+        for (int i = b; i > 3; i--)
+        {
+            c = c * 10;
+        }
+        result = (a / c) % 10;
+    }
+return result;
+}
---
+++ Task15/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+Console.Write("Введите номер дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day > 0 && day < 7 || day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+        Console.WriteLine("Будний день, еще работать и работать");   
+    else if (day == 6 || day == 7)
+        Console.WriteLine("Ура, выходной!");
+   
+else
+    Console.WriteLine("Это не день недели, смотри что вводишь!");
